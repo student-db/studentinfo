53f6e6e53263a07f60024fc78da2d4803f5b0a97
Nithin S V <nithinsurendran321@gamil.com>
2021-01-19 18:19:59 +0530

    update

--- studentinfo/addmark.php
+++ studentinfo/addmark.php
@@ -17,26 +17,26 @@
 		<th>Batch</th><td><input type="text" name="batch"></td>
 	</tr>
 	<tr>
-		<th>DBMS</th><td><input type="text" name="sub1"></td>
+		<th>DBMS</th><td><input type="number" name="sub1"></td>
 	</tr>
 	<tr>
-		<th>CN</th><td><input type="text" name="sub2"></td>
+		<th>CN</th><td><input type="number" name="sub2"></td>
 	</tr>
 	<tr>
-		<th>WEB</th><td><input type="text" name="sub3"></td>
+		<th>WEB</th><td><input type="number" name="sub3"></td>
 	</tr>
 	<tr>
-		<th>SE</th><td><input type="text" name="sub4"></td>
+		<th>SE</th><td><input type="number" name="sub4"></td>
 	</tr>
 	<tr>
-		<th>DAA</th><td><input type="text" name="sub5"></td>
+		<th>DAA</th><td><input type="number" name="sub5"></td>
 	</tr>
 	
 	<tr>
-		<th>Lab 1</th><td><input type="text" name="lab1"></td>
+		<th>Lab 1</th><td><input type="number" name="lab1"></td>
 	</tr>
 	<tr>
-		<th>Lab 2</th><td><input type="text" name="lab2"></td>
+		<th>Lab 2</th><td><input type="number" name="lab2"></td>
 	</tr>
 	<tr>
 		<td></td><td><input type="submit" name="submit" value="Add marks"class="btn btn-success" ></td>
@@ -61,8 +61,9 @@ if (isset($_POST['submit'])) {
 	
 	$j=$_POST['lab1'];
 	$k=$_POST['lab2'];
+	$t=$d+$e+$f+$g+$h+$j+$k;
 	
-	$q="INSERT INTO `marks`(`rollno`, `name`, `batch`, `sub1`, `sub2`, `sub3`, `sub4`, `sub5`, `lab1`, `lab2`) VALUES ('$a','$b','$c','$d','$e','$f','$g','$h','$i','$j','$k')";
+	$q="INSERT INTO `marks`(`rollno`, `name`, `batch`, `sub1`, `sub2`, `sub3`, `sub4`, `sub5`, `lab1`, `lab2`,`tot`) VALUES ('$a','$b','$c',$d,$e,$f,$g,$h,$i,$j,$k,$t)";
 	$ex=mysqli_query($con,$q);
 	if ($ex) {
 		echo "<script>alert('Marks Added');window.location.href='mark.php'</script>";
